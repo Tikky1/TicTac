563df263a359b38b04acf527ae864fa9ebaf5e04
Tikky1 <efekankul5161@gmail.com>
2025-03-13 10:50:20 +0300

    First

---
+++ TicTacToe/CheckGameFinish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class CheckGameFinish
+    {
+
+
+        public void isGameFinished(Player player)
+        {
+            bool isGameFinished = false;
+
+            Tile[] tiles = player.getPlayerTileArray();
+
+            if (tiles[0].getTileX(). == tiles[1].getTileX() == tiles[2].getTileX())
+            {
+                isGameFinished = !isGameFinished;
+            }
+
+
+        }
+    }
+}
